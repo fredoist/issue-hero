f2f0c8441baed0adc9a7824e2552deae946382c2
Freddy González <7684330+fredoist@users.noreply.github.com>
2023-01-31 11:00:33 -0600

    chore: setup monorepo

---
+++ bot/src/index.cjs
@@ -0,0 +1,9 @@
+const { Probot } = require('probot') // eslint-disable-line no-unused-vars
+
+/**
+ * App entry point
+ * @param {Probot} app The Probot context
+ */
+module.exports = (app) => {
+  app.log('Yay, the app was loaded!')
+}
---
+++ bot/src/utils.cjs
@@ -0,0 +1,2 @@
+const cohere = require('cohere-ai')
+cohere.init(process.env.COHERE_API_KEY)
