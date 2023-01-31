b99770c4a4d1b4b465a110b268a2769cbd4f9255
Freddy González <7684330+fredoist@users.noreply.github.com>
2023-01-31 13:43:49 -0600

    feat(bot): add summarize and labelling

--- bot/src/index.cjs
+++ bot/src/index.cjs
@@ -1,4 +1,5 @@
 const { Probot } = require('probot') // eslint-disable-line no-unused-vars
+const { summarize, label } = require('./utils.cjs')
 
 /**
  * App entry point
@@ -6,4 +7,29 @@ const { Probot } = require('probot') // eslint-disable-line no-unused-vars
  */
 module.exports = (app) => {
   app.log('Yay, the app was loaded!')
+  app.on('issues.opened', async (context) => {
+    const { title, body, labels: currentLabels, assignees: currentAssignees } = context.payload.issue // eslint-disable-line no-unused-vars
+
+    try {
+      const summary = await summarize(`${title.trim()}. ${body.trim()}`)
+      const labels = await label(body)
+
+      const commentBody = `TLDR: ${summary}
+
+---
+
+We suggest the following labels based on the issue content:
+${labels.map(({ label, confidence }) => `- ${label} (\`${(confidence * 100).toFixed(2)}%\`)`).join('\n')}
+`
+      const comment = context.issue({ body: commentBody })
+      const _labels = context.issue({ labels: labels.map(({ label }) => label) })
+
+      await context.octokit.issues.createComment(comment)
+      await context.octokit.issues.addLabels(_labels)
+      // await context.octokit.issues.addAssignees(assignees)
+      app.log('New response for issue')
+    } catch (err) {
+      app.log.error(err)
+    }
+  })
 }
--- bot/src/utils.cjs
+++ bot/src/utils.cjs
@@ -1,2 +1,44 @@
 const cohere = require('cohere-ai')
 cohere.init(process.env.COHERE_API_KEY)
+
+/**
+ * A function that takes in a text and returns a summary of the text.
+ * @param {string} text - The text to summarize
+ * @returns {Promise<string>} - The summary of the text
+ */
+exports.summarize = async (text) => {
+  try {
+    const { body: { generations: [{ text: summary }] } } = await cohere.generate({
+      model: 'xlarge',
+      prompt: text,
+      max_tokens: 40,
+      temperature: 0.3,
+      stop_sequences: ['--'],
+      p: 0.75,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.1,
+      preset: 'Issue-Summary-sgyrr0'
+    })
+    return summary
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+/**
+ * A function that takes in a text and returns an array of labels.
+ * @param {string} context - The context of the issue
+ * @returns {Promise<{label: string, confidence: number}[]>} - The labels of the issue
+ */
+exports.label = async (context) => {
+  try {
+    const { body: { classifications: [{ labels }] } } = await cohere.classify({
+      model: 'large',
+      inputs: [context],
+      preset: 'Issue-Labeller-xp8hxd'
+    })
+    return Object.entries(labels).map(([label, { confidence }]) => ({ label, confidence })).filter(({ confidence }) => confidence > 0.85)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
