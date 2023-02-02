98216624387a578bde1ebbf6b745b0a015e06f64
Freddy González <7684330+fredoist@users.noreply.github.com>
2023-02-02 11:14:12 -0600

    feat(bot): add support for custom config

--- bot/src/index.cjs
+++ bot/src/index.cjs
@@ -1,24 +1,32 @@
-const { summarize, label, isSpam } = require('./ai.cjs');
-const { createComment } = require('./utils.cjs');
+const { summarize, label, isSpam } = require('./ai.cjs')
+const { createComment, getConfig, saveConfig, redis } = require('./utils.cjs')
 
 /**
  * App entry point
  * @param {import('probot').Probot} app - The Probot context
+ * @param {import('probot').ApplicationFunctionOptions} options - The Probot server options
  *
  * @todo Add support for assignees
- * @todo Add support for custom settings
  * @todo Add support for duplicated issues
  */
-module.exports = (app) => {
+module.exports = async (app, { getRouter }) => {
+  await redis.connect()
+
   app.on('issues.opened', async (context) => {
-    const { title, body, labels: currentLabels, assignees: currentAssignees } = context.payload.issue // eslint-disable-line no-unused-vars
+    const {
+      title,
+      body,
+      labels: currentLabels,
+      assignees: currentAssignees,
+    } = context.payload.issue // eslint-disable-line no-unused-vars
 
     try {
+      const config = await getConfig(context.payload.repository.full_name)
       const isToxic = await isSpam(`${title.trim()}. ${body.trim()}`)
       const summary = await summarize(`${title.trim()}. ${body.trim()}`)
       const labels = await label(body)
 
-      const comment = context.issue({ body: createComment(summary, labels, isToxic) })
+      const comment = context.issue({ body: createComment(summary, labels, isToxic, config) })
       await context.octokit.issues.createComment(comment)
 
       if (isToxic) {
@@ -29,6 +37,43 @@ module.exports = (app) => {
       await context.octokit.issues.addLabels(labels.map(({ label }) => label))
     } catch (err) {
       app.log.error(err)
+    }
+  })
+
+  const router = getRouter('/api')
+
+  router.use(require('express').json())
+  router.use((req, res, next) => {
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'http://0.0.0.0:3000'
+    ]
+    if (!allowedOrigins.includes(req.headers.origin)) {
+      res.status(401).send('Unauthorized')
+      return
+    }
+
+    next()
+  })
+
+  router.get('/config/*', async (req, res) => {
+    try {
+      const repo = req.params[0]
+      const config = await getConfig(repo) || {}
+      res.status(200).json(config)
+    } catch (err) {
+      app.log.error(err)
+      res.status(500).send('Internal Server Error')
+    }
+  })
+  router.patch('/config/*', async (req, res) => {
+    try {
+      const repo = req.params[0]
+      const config = await saveConfig(repo, req.body)
+      res.status(200).json(config)
+    } catch (err) {
+      app.log.error(err)
+      res.status(500).send('Internal Server Error')
     }
   })
 }
