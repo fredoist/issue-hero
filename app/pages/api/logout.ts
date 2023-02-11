772dd99847a9cf65d446923e3334d2956b4afdcf
Freddy González <7684330+fredoist@users.noreply.github.com>
2023-02-11 23:15:45 +0000

    refactor!: remove files from previous codebase

--- app/pages/api/logout.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiHandler } from 'next'
-
-const handler: NextApiHandler = async (req, res) => {
-  res.setHeader('Set-Cookie', `token=; Path=/; HttpOnly; SameSite=Lax; Secure=true;`)
-  res.redirect('/')
-}
-
-export default handler
