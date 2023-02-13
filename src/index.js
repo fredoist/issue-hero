f8e990091c1a9f272684801a81c0f3d72b097637
Freddy González <7684330+fredoist@users.noreply.github.com>
2023-02-13 22:18:08 +0000

    feat: adjust actions to configuration

--- src/index.js
+++ src/index.js
@@ -20,22 +20,26 @@ module.exports = (app) => {
     }
     try {
       const config = await getConfig({ context, owner, repo })
-      const isSpam = await spamFilter({ title, body })
-      if (isSpam) {
-        return createComment({
+      if (config.spam.enabled) {
+        const isSpam = await spamFilter({ title, body, confidence: config.spam.confidence })
+        if (isSpam) {
+          return createComment({
+            context,
+            body: 'Issue Hero thinks this issue is spam'
+          })
+        }
+      }
+      if (config.summary.enabled) {
+        const summary = body.length > 0 ? await summarize({ body, threshold: config.summary.threshold }) : title
+        createComment({
           context,
-          body: 'Issue Hero thinks this issue is spam'
+          body: `TLDR: ${summary}`
         })
       }
-      const summary = await summarize({ title, body })
-      const labelSuggestions = await label({ title, body, labels })
-      createComment({
-        context,
-        body: `TLDR: ${summary}`
-      })
-      await context.octokit.issues.addLabels(context.issue({
-        labels: [...new Set([...labels, ...labelSuggestions])]
-      }))
+      if (config.label.enabled) {
+        const suggestedLabels = await label({ body, labels, confidence: config.label.confidence })
+        await context.octokit.issues.addLabels(context.issue({ labels: suggestedLabels }))
+      }
     } catch (error) {
       app.log.error(error)
     }
@@ -43,19 +47,48 @@ module.exports = (app) => {
 }
 
 /**
+ * @typedef Config
+ * @property {Object} summary
+ * @property {boolean} summary.enabled
+ * @property {number} summary.threshold
+ * @property {Object} label
+ * @property {boolean} label.enabled
+ * @property {number} label.confidence
+ * @property {Object} spam
+ * @property {boolean} spam.enabled
+ * @property {number} spam.confidence
+ * @property {string[]} spam.notify
+ */
+/**
  * Get the configuration for the repository
  * @param {Object} props
  * @param {Context<'issues.opened'>} props.context - The context of the issue
  * @param {string} props.owner - The owner of the repository
  * @param {string} props.repo - The name of the repository
+ * @returns {Promise<Config>}
  */
 async function getConfig ({ context, owner, repo }) {
+  const defaultConfig = {
+    summary: {
+      enabled: true,
+      threshold: 100
+    },
+    label: {
+      enabled: true,
+      confidence: 0.85
+    },
+    spam: {
+      enabled: true,
+      confidence: 0.85,
+      notify: []
+    }
+  }
   const { data: { content } } = await context.octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
     owner,
     repo,
     path: 'issue-hero.yml'
   })
-  return content ? parse(Buffer.from(content, 'base64').toString()) : {}
+  return content ? parse(Buffer.from(content, 'base64').toString()) : defaultConfig
 }
 
 /**
@@ -72,14 +105,13 @@ function createComment ({ context, body }) {
 /**
  * Generate an AI summary of the issue
  * @param {Object} props
- * @param {string} props.title - The title of the issue
  * @param {string} props.body - The body of the issue
  * @returns {Promise<string>}
  */
-async function summarize ({ title, body }) {
+async function summarize ({ body, threshold }) {
   const result = await cohere.generate({
-    prompt: `${title}\n${body}`,
-    max_tokens: 100,
+    prompt: body,
+    max_tokens: threshold,
     model: 'command-xlarge-nightly',
     preset: 'Issue-Summary-Command-sgyrr0'
   })
@@ -90,37 +122,34 @@ async function summarize ({ title, body }) {
 /**
  * Check if the issue is spam
  * @param {Object} props
- * @param {string} props.title - The title of the issue
  * @param {string} props.body - The body of the issue
  * @returns {Promise<boolean>}
  */
-async function spamFilter ({ title, body }) {
+async function spamFilter ({ body, confidence }) {
   const result = await cohere.classify({
-    model: 'large',
-    inputs: [`${title}\n${body}`],
-    preset: 'Product-Review-Sentiment-kdtu14'
+    model: process.env.SPAM_DETECTION_MODEL,
+    inputs: [body]
   })
-  const { confidence, prediction } = result.body.classifications[0]
-  return confidence > 0.85 && prediction === 'negative'
+  const { confidence: aiConfidence, prediction } = result.body.classifications[0]
+  return aiConfidence > confidence && prediction === 'spam'
 }
 
 /**
  * Get AI label suggestions for the issue
  * @param {Object} props
- * @param {string} props.title - The title of the issue
  * @param {string} props.body - The body of the issue
  * @param {string[]} props.labels - The labels of the issue
  * @returns {Promise<string[]>}
  */
-async function label ({ title, body, labels }) {
+async function label ({ body, labels, confidence }) {
   const result = await cohere.classify({
-    inputs: [`${title}\n${body}`],
-    model: 'large',
-    preset: 'Issue-Labeller-xp8hxd'
+    inputs: [body],
+    model: process.env.ISSUE_LABEL_MODEL
   })
 
   const { labels: suggestions } = result.body.classifications[0]
-  return Object.entries(suggestions).map(([label, { confidence }]) => {
-    return confidence > 0.85 ? label : null
+  const suggestedLabels = Object.entries(suggestions).map(([label, { confidence: aiConfidence }]) => {
+    return aiConfidence > confidence ? label : null
   }).filter(Boolean)
+  return [...new Set([...labels, ...suggestedLabels])]
 }
