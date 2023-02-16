7157e59e6a335bc3daf6e4e53be9557a1b45352f
Freddy González <7684330+fredoist@users.noreply.github.com>
2023-02-16 19:05:05 +0000

    feat: cancel remaining promises if it's spam

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { Probot } from 'probot'
+import { Promise as BPromise } from 'bluebird'
 import { addLabels, createSummary, filterSpam } from './models'
+
+BPromise.config({ cancellation: true })
 
 export default (app: Probot) => {
   app.on('issues.opened', async (context) => {
     const { private: isPrivateRepo } = context.payload.repository
     if (isPrivateRepo) return
     try {
-      return await Promise.all([filterSpam, createSummary, addLabels].map((fn) => fn(context)))
+      return BPromise.all([filterSpam, createSummary, addLabels].map((fn) => fn(context)))
     } catch (error) {
       return error && context.log.error(error)
     }
